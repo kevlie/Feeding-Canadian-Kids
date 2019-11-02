edebff90fb8ce3b4e657d1eb82fca1599451f81b
wang-ximing <wxm19990222love@sina.com>
2019-11-02 16:16:35 -0400

    added sign up buttons

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { Route, Redirect } from 'react-router'
 import {Form, Button} from "react-bootstrap";
 import "./Login.css";
 
@@ -10,47 +11,68 @@ function Login(props){
 	function checkValidation(){
 		let validate=false;
 		if (email==="admin" && password==="admin"){validate=true}
-		else if (email==="user" && password==="user"){validate=true}
+		else if (email==="program" && password==="program"){validate=true}
+		else if (email==="restaurant" && password==="restaurant"){validate=true}
 		return validate
 	}
 	function handleLogin(e){
 		e.preventDefault();
 		if (!checkValidation()){setError("invalid email or password!")}
-		//else{//direct to the user page or admin page }
+		//else if (email==="program" && password==="program"){
+		// 	props.history.push(//program page)
+		//}
 	}
 
+	function handleProgram(e){
+		e.preventDefault();
+		props.history.push("/programRegistration")
+		
+	}
+
+	function handleRestaurant(e){
+		e.preventDefault();
+		props.history.push("/restaurantRegistration")
+	}
+	
 	return (
 		<div className="page">
-		<div className='loginForm'>
-			<p>For testing: user/user or admin/admin></p>
-			<p className="error">{error}</p>
-			<Form>
-			  <Form.Group controlId="formBasicEmail">
-			    <Form.Label>Email</Form.Label>
-			    <Form.Control 
-			    	autoFocus
-			    	type="text" 
-			    	placeholder="Enter your email"
-			    	value={email}
-			    	onChange={e => setEmail(e.target.value)}
-			    />
-			  </Form.Group>
+			<div className='loginForm'>
+				<em>For testing: program/program or restaurant/restaurant or admin/admin></em>
+				<p className="error">{error}</p>
+				<Form>
+				  <Form.Group controlId="formBasicEmail">
+				    <Form.Label>Email</Form.Label>
+				    <Form.Control 
+				    	autoFocus
+				    	type="text" 
+				    	placeholder="Enter your email"
+				    	value={email}
+				    	onChange={e => setEmail(e.target.value)}
+				    />
+				  </Form.Group>
 
-			  <Form.Group controlId="formBasicPassword">
-			    <Form.Label>Password</Form.Label>
-			    <Form.Control 
-			    	type="password" 
-			    	placeholder="Enter your password"
-			    	value={password}
-			    	onChange={e => setPassword(e.target.value)}
-			    />
-			  </Form.Group>
+				  <Form.Group controlId="formBasicPassword">
+				    <Form.Label>Password</Form.Label>
+				    <Form.Control 
+				    	type="password" 
+				    	placeholder="Enter your password"
+				    	value={password}
+				    	onChange={e => setPassword(e.target.value)}
+				    />
+				  </Form.Group>
 
-			  <Button variant="primary" type="submit" onClick={handleLogin}>
-			    Submit
-			  </Button>
-			</Form>
-      </div>
+				  <Button variant="primary" type="submit" onClick={handleLogin}>
+				    Login
+				  </Button>
+				  <p className="newuser">New User? Register Now!</p>
+				  <Button style={{marginRight: "20px"}}variant="primary" type="submit" onClick={handleProgram}>
+				    Register as a Program
+				  </Button>
+				  <Button variant="primary" type="submit" onClick={handleRestaurant}>
+				    Register as a Restaurant
+				  </Button>
+				</Form>
+	      </div>
       </div>
 	);
 }
