f12804df37388a6655a63d8032f173948134d44c
Kelvin Fan <kelvinfan001@gmail.com>
2019-11-03 17:27:29 -0500

    connected express with react

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import RestaurantRegistration from "./components/RestaurantRegistration/RestaurantRegistration.component";
 import ProgramRegistration from "./components/ProgramRegistration/ProgramRegistration.component";
@@ -11,25 +11,44 @@ function HomePage() {
     <h1>Feeding Canadian Kids</h1>
   );
 }
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/login' component={Login} />
-        <Route
-          exact
-          path="/restaurantRegistration"
-          component={RestaurantRegistration}
-        />
-        <Route
-          exact
-          path="/programRegistration"
-          component={ProgramRegistration}
-        />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => err);
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/login' component={Login} />
+          <Route
+            exact
+            path="/restaurantRegistration"
+            component={RestaurantRegistration}
+          />
+          <Route
+            exact
+            path="/programRegistration"
+            component={ProgramRegistration}
+          />
+        </Switch>
+        <p className="App-intro">{this.state.apiResponse}</p>
+      </div >
+    );
+  }
 }
 
 export default App;
