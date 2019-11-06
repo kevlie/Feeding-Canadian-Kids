ec7bb143beb24cc62d59093632c2a2acec59d5a8
Dhruvasu <dhruvasu1@hotmail.com>
2019-11-06 04:26:21 -0500

    added admin home page

---
+++ react_app/src/components/Admin/Admin.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Sidebar from "./Sidebar";
+import "./Admin.css";
+
+class Admin extends React.Component {
+	render() {
+		return (
+			<div>
+				<Sidebar />
+				<div class="jumbotron jumbotron-fluid">
+				  <div class="container">
+				    <h1 class="display-4">Welcome Admin</h1>
+				    <p class="lead">This is the Admin's Portal. Here you can manage new partner
+				    				sign up requests, view existing partners' info, and pair 
+				    				a program partner with a restaurant.</p>
+				  </div>
+				</div>
+
+				<div class="tab">
+					<div id="number" class="programText">10</div>
+					<div id="program" class="programText">Active Programs</div>
+				</div>
+
+				<div class="tab">
+					<div id="number" class="programText">20</div>
+					<div id="program" class="programText">Active Restaurants</div>
+				</div>
+
+				<div class="tab">
+					<div id="number" class="programText">3</div>
+					<div id="program" class="programText">New Sign-Ups</div>
+				</div>
+			</div>
+		)
+	}
+}
+
+export default Admin;
