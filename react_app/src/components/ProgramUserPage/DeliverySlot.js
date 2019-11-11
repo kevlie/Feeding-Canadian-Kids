da88eb37278f43c8faf3f8b0815a830fe64d1bfe
Stephen <stephen.utama@mail.utoronto.ca>
2019-11-10 20:48:04 -0500

    added contactphone and contactperson to programuser

--- react_app/src/components/ProgramUserPage/DeliverySlot.js
+++ react_app/src/components/ProgramUserPage/DeliverySlot.js
@@ -10,22 +10,30 @@ class DeliverySlot extends React.Component {
         {
           Restaurant: "Taylor's Landing",
           Meals: "34",
-          Address: "150 Grenoble Drive, North York, ON M3C 1E3"
+          Address: "150 Grenoble Drive, North York, ON M3C 1E3",
+          Contact: "Resto: 647-942-6318",
+          POC: "Chef, Winston Brown"
         },
         {
           Restaurant: "Harper's Landing",
           Meals: "30",
-          Address: "485 Kerr St, Oakville, ON L6K 3C5"
+          Address: "485 Kerr St, Oakville, ON L6K 3C5",
+          Contact: "Resto: 905-338-0707",
+          POC: "Chloe Catell, Assitant GM & Faz, Chef"
         },
         {
           Restaurant: "Jackson's Landing",
           Meals: "50",
-          Address: "4228 New St, Burlington, ON L7L 1T3"
+          Address: "4228 New St, Burlington, ON L7L 1T3",
+          Contact: "Resto: 905-632-5302, Cell: 437-996-8668",
+          POC: "Tahir, Front of House Lead"
         },
         {
           Restaurant: "Hunter's Landing",
           Meals: "18",
-          Address: "20 Portugal Square M6J 3P2"
+          Address: "20 Portugal Square M6J 3P2",
+          Contact: "647-352-6082 ext4",
+          POC: "Jesse Topliffe Drew Colin Anthony"
         }
       ]
     };
@@ -65,6 +73,12 @@ class DeliverySlot extends React.Component {
             </h3>
             <h3>
               {"Address: "} {info.Address}
+            </h3>
+            <h3>
+              {"Contact Phone: "} {info.Contact}
+            </h3>
+            <h3>
+              {"Contact Person: "} {info.POC}
             </h3>
           </Modal.Body>
         </Modal>
