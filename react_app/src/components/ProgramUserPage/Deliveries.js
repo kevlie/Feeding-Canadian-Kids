93e823104a2267cdd66fc433734ec2cb86df1294
Stephen <stephen.utama@mail.utoronto.ca>
2019-11-10 20:12:13 -0500

    added files for program user page

---
+++ react_app/src/components/ProgramUserPage/Deliveries.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Table } from "react-bootstrap";
+import "./Deliveries.css";
+import DeliverySlot from "./DeliverySlot.js";
+
+class Deliveries extends React.Component {
+  render() {
+    return (
+      <>
+        <h1> Your Orders for the Week</h1>
+        <h2> Click on an order below to see further information</h2>
+        <Table striped bordered size="sm">
+          <thead>
+            <tr>
+              <th>Monday</th>
+              <th>Tuesday</th>
+              <th>Wednesday</th>
+              <th>Thursday</th>
+              <th>Friday</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <DeliverySlot time="5:30" which="0" />
+                <DeliverySlot time="6:00" which="1" />
+              </td>
+              <td></td>
+              <td>
+                <DeliverySlot time="5:00" which="2" />
+              </td>
+              <td></td>
+              <td>
+                <DeliverySlot time="7:00" which="3" />
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </>
+    );
+  }
+}
+export default Deliveries;
