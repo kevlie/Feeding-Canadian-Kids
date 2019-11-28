c28d126721cfb12c7b617cfca220349a515f915b
faroo127 <ibrahim.farooq@mail.utoronto.ca>
2019-11-27 22:58:10 -0500

    Added route to get order information

---
+++ express_server/routes/restaurantUserPage.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var express = require('express');
+var restaurantRouter = express.Router();
+const sql = require('../db.js');
+
+restaurantRouter.post('/orders', function(req, res) {
+    let email = req.body.restaurantEmail;
+    console.log(email)
+    let query1 = 'SELECT pr.name, pr.address, pr.dietary_restrictions, monday_meals, monday_time, ' +
+                 'tuesday_meals, tuesday_time, wednesday_meals, wednesday_time, ' +
+                 'thursday_meals, thursday_time, friday_meals, friday_time ' + 
+                 'FROM restaurant_partners r INNER JOIN pairings p ON r.restaurant_id = p.restaurant_id ' + 
+                 'INNER JOIN program_partners pr ON pr.program_id = p.program_id WHERE r.contact_email = ?'
+    if (email){
+        sql.query(query1, [email], (err, results) => {
+            if (err){
+                return res.state(500).send(err);
+            }
+            if (results.length >= 0){
+                //may have multiple rows now
+                res.status(200).send(results) //results is an array where each object is a row from the query
+
+            }
+        })
+    }
+})
+
+module.exports = restaurantRouter;
