29ebf344e2ff6c871775ea83671058270c5633b1
faroo127 <ibrahim.farooq@mail.utoronto.ca>
2019-11-28 17:04:25 -0500

    Added restaurant name to the welcome message for restaurant users

--- express_server/routes/restaurantUserPage.js
+++ express_server/routes/restaurantUserPage.js
@@ -6,7 +6,7 @@ const sql = require('../db.js');
 
 restaurantRouter.post('/orders', function(req, res) {
     let email = req.body.restaurantEmail;
-    console.log(email)
+    //console.log(email)
     let query1 = 'SELECT pr.name, pr.address, pr.dietary_restrictions, monday_meals, monday_time, ' +
                  'tuesday_meals, tuesday_time, wednesday_meals, wednesday_time, ' +
                  'thursday_meals, thursday_time, friday_meals, friday_time ' + 
@@ -15,12 +15,27 @@ restaurantRouter.post('/orders', function(req, res) {
     if (email){
         sql.query(query1, [email], (err, results) => {
             if (err){
-                return res.state(500).send(err);
+                return res.status(500).send(err);
             }
             if (results.length >= 0){
                 //may have multiple rows now
                 res.status(200).send(results) //results is an array where each object is a row from the query
 
+            }
+        })
+    }
+})
+
+restaurantRouter.post('/name', function(req,res) {
+    let email = req.body.restaurantEmail;
+    let query1 = 'SELECT name FROM restaurant_partners WHERE restaurant_partners.contact_email = ?'
+    if (email){
+        sql.query(query1, [email], (err, results) => {
+            if (err){
+                return res.status(500).send(err);
+            }
+            if (results.length >= 0) {
+                res.status(200).send(results)
             }
         })
     }
