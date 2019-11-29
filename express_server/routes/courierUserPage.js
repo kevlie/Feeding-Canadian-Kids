43e92f107dc50ec09de6d109d68122c30ae84d48
Stephen <stephen.utama@mail.utoronto.ca>
2019-11-29 01:02:44 -0500

    courier name now shoes in courieruserpage

--- express_server/routes/courierUserPage.js
+++ express_server/routes/courierUserPage.js
@@ -14,4 +14,19 @@ courierRouter.post("/", function(req, res) {
   });
 });
 
+courierRouter.post("/name", function(req, res) {
+  let email = req.body.email;
+  let query2 =
+    "SELECT name FROM courier_partners WHERE courier_partners.email = ?";
+  if (email) {
+    sql.query(query2, email, (err, results) => {
+      if (err) {
+        return res.status(500).send(err);
+      }
+      if (results.length >= 0) {
+        res.json(results);
+      }
+    });
+  }
+});
 module.exports = courierRouter;
--- react_app/src/components/CourierUserPage/CourierUserPage.js
+++ react_app/src/components/CourierUserPage/CourierUserPage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Row, Col, Nav, Tab } from "react-bootstrap";
+import { Row, Col, Nav, Tab, Tabs } from "react-bootstrap";
+
 import { withRouter } from "react-router-dom";
 import WelcomeMessage from "./WelcomeMessage.js";
 // import ProgramsPartners from "./ProgramsPartners.js";
@@ -9,10 +10,26 @@ class CourierUserPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      name: "default",
       email: this.props.history.location.state.email,
       approval_status: 0
     };
   }
+  // getCourierName = data => {
+  //   return new Promise(function(resolve, reject) {
+  //     fetch("http://localhost:9000/api/courieruserpage/name", {
+  //       method: "post",
+  //       headers: {
+  //         Accept: "application/json",
+  //         "Content-Type": "application/json"
+  //       },
+  //       credentials: "include",
+  //       body: JSON.stringify(data)
+  //     }).then(res => {
+  //       resolve(res.json());
+  //     });
+  //   });
+  // };
   componentDidMount = () => {
     document.body.classList.add("hunnid");
     document.documentElement.classList.add("hunnid");
@@ -29,6 +46,29 @@ class CourierUserPage extends React.Component {
       .then(json => {
         this.setState({ approval_status: json[0].approval_status });
       });
+
+    // this.getCourierName({ email: this.state.email })
+    //   .then(value => {
+    //     this.setState({ name: value[0].name });
+    //     return this.state.name;
+    //   })
+    //   .then(value => {
+    //     console.log(value);
+    //   });
+    fetch("http://localhost:9000/api/courieruserpage/name", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      credentials: "include",
+      body: JSON.stringify({ email: this.state.email })
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ name: json[0].name });
+        console.log(this.state.name);
+      });
   };
 
   componentWillUnmount() {
@@ -38,6 +78,7 @@ class CourierUserPage extends React.Component {
 
   render() {
     console.log(this.state.approval_status);
+    console.log(this.state.name);
     return (
       <>
         {this.state.approval_status === 0 ? (
@@ -46,45 +87,60 @@ class CourierUserPage extends React.Component {
             work to get back to you within 48 business hours.
           </h4>
         ) : (
-          <div className="trial">
-            <Tab.Container
-              id="left-tabs-example"
-              defaultActiveKey="first"
-              className="no-scroll m-height"
-            >
-              <Row className="no-scroll m-height">
-                <Col sm={2} className="pill-tabs-color m-height">
-                  <Nav variant="pills" className="flex-column pill-tabs">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">{this.state.email}</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">Your Deliveries</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Your Partners</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                </Col>
-                <Col sm={10} className="m-height">
-                  <Tab.Content className="m-height">
-                    <Tab.Pane eventKey="first" className="m-height">
-                      <WelcomeMessage email={this.state.email} />
-                    </Tab.Pane>
+          <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+            <Tab eventKey="home" title="Home">
+              <WelcomeMessage name={this.state.name} email={this.state.email} />
+            </Tab>
+            <Tab eventKey="onboarding" title="Onboarding Guide">
+              {/* <ProgramTraining /> */}
+            </Tab>
+            <Tab eventKey="deliveries" title="Your Deliveries">
+              <div className="order-div">{/* <Deliveries /> */}</div>
+            </Tab>
+            <Tab eventKey="restaurants" title="Your Restaurants">
+              {/* <RestaurantsPartners/> */}
+            </Tab>
+          </Tabs>
 
-                    <Tab.Pane eventKey="second" className="m-height">
-                      <div className="order-div">
-                        {/* <Orders email={this.state.email} /> */}
-                      </div>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third" className="m-height">
-                      {/* <ProgramsPartners /> */}
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Col>
-              </Row>
-            </Tab.Container>
-          </div>
+          // <div className="trial">
+          //   <Tab.Container
+          //     id="left-tabs-example"
+          //     defaultActiveKey="first"
+          //     className="no-scroll m-height"
+          //   >
+          //     <Row className="no-scroll m-height">
+          //       <Col sm={2} className="pill-tabs-color m-height">
+          //         <Nav variant="pills" className="flex-column pill-tabs">
+          //           <Nav.Item>
+          //             <Nav.Link eventKey="first">{this.state.email}</Nav.Link>
+          //           </Nav.Item>
+          //           <Nav.Item>
+          //             <Nav.Link eventKey="second">Your Deliveries</Nav.Link>
+          //           </Nav.Item>
+          //           <Nav.Item>
+          //             <Nav.Link eventKey="third">Your Partners</Nav.Link>
+          //           </Nav.Item>
+          //         </Nav>
+          //       </Col>
+          //       <Col sm={10} className="m-height">
+          //         <Tab.Content className="m-height">
+          //           <Tab.Pane eventKey="first" className="m-height">
+          //             <WelcomeMessage email={this.state.email} />
+          //           </Tab.Pane>
+
+          //           <Tab.Pane eventKey="second" className="m-height">
+          //             <div className="order-div">
+          //               {/* <Orders email={this.state.email} /> */}
+          //             </div>
+          //           </Tab.Pane>
+          //           <Tab.Pane eventKey="third" className="m-height">
+          //             {/* <ProgramsPartners /> */}
+          //           </Tab.Pane>
+          //         </Tab.Content>
+          //       </Col>
+          //     </Row>
+          //   </Tab.Container>
+          // </div>
         )}
       </>
     );
