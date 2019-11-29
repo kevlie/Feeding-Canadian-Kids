463add0710bb5ec4f3bc84f77286290c482a7943
kevlie <kevinlie10@gmail.com>
2019-11-28 19:00:22 -0500

    did not use table bootstrap

--- react_app/src/components/ProgramUserPage/RestaurantsPartners.js
+++ react_app/src/components/ProgramUserPage/RestaurantsPartners.js
@@ -1,4 +1,5 @@
 import { BootstrapTable, TableHeaderColumn } from "react-bootstrap-table";
+import { Table } from "react-bootstrap";
 import React from "react";
 
 const columns = [
@@ -24,7 +25,7 @@ class RestaurantsPartners extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      rows: []
     };
   }
 
@@ -32,40 +33,35 @@ class RestaurantsPartners extends React.Component {
     fetch("http://localhost:9000/api/programPairings")
       .then(res => res.json())
       .then(data => {
-        let test1 = {
-          name: "k3",
-          address: "mkk",
-          contact_email: "kwada",
-          phone: "adwa"
-        };
-        let test2 = {
-          name: "k3213",
-          address: "mk32k",
-          contact_email: "kw23ada",
-          phone: "a23dwa"
-        };
-        let test3 = {
-          name: "k23",
-          address: "m23kk",
-          contact_email: "kw23ada",
-          phone: "a12dwa"
-        };
-        this.setState({ data });
+        let rows = [];
+        for (let i = 0; i < data.length; i++) {
+          let rowData = [];
+          rowData.push(<td> {data[i].name} </td>);
+          rowData.push(<td> {data[i].address} </td>);
+          rowData.push(<td> {data[i].contact_email} </td>);
+          rowData.push(<td> {data[i].phone} </td>);
+          rows.push(<tr> {rowData} </tr>);
+        }
+        this.setState({ rows });
       });
   }
 
   render() {
     return (
-      <BootstrapTable data={this.state.data}>
-        <TableHeaderColumn dataField="name" isKey>
-          Restaurant Name
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="address">Address</TableHeaderColumn>
-        <TableHeaderColumn dataField="contact_email">
-          Contact Email
-        </TableHeaderColumn>
-        <TableHeaderColumn dataField="phone">Phone Number</TableHeaderColumn>
-      </BootstrapTable>
+      <div>
+        <h1> Your Restaurnt Partners Information</h1>
+        <Table striped bordered>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>{this.state.rows}</tbody>
+        </Table>
+      </div>
     );
   }
 }
