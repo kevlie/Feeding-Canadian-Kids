52401ab76840c9ca31f9160c3f6bf1ad157e74fb
Stephen <stephen.utama@mail.utoronto.ca>
2019-11-30 17:31:26 -0500

    pending approval page

---
+++ react_app/src/components/PendingApproval/PendingApproval.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class PendingApproval extends React.Component {
+  render() {
+    return (
+      <h4>
+        Your application is still currently being processed. Our staffs will
+        work to get back to you within 48 business hours.
+      </h4>
+    );
+  }
+}
+
+export default PendingApproval;
