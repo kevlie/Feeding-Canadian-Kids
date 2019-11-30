d7739d45fe23d3a48753c9e763d021e1b131ba4f
kevlie <kevinlie10@gmail.com>
2019-11-30 18:09:43 -0500

    Connected restaurant user page to backend

--- react_app/src/components/ProgramUserPage/ProgramUserPage.js
+++ react_app/src/components/ProgramUserPage/ProgramUserPage.js
@@ -8,87 +8,104 @@ import ProgramTraining from "./ProgramTraining.js";
 import "./ProgramUserPage.css";
 
 class ProgramUserPage extends React.Component {
-  // componentDidMount() {
-  //   document.body.classList.add("hunnid");
-  //   document.documentElement.classList.add("hunnid");
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Sample name"
+    };
+  }
 
-  // componentWillUnmount() {
-  //   document.body.classList.remove("hunnid");
-  //   document.documentElement.classList.remove("hunnid");
-  // }
+  componentDidMount() {
+    document.body.classList.add("hunnid");
+    document.documentElement.classList.add("hunnid");
+    fetch("http://localhost:9000/api/programuserpage/name", {
+      method: "get",
+      credentials: "include"
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({
+          name: json[0].name
+        });
+      });
+  }
 
-  // render() {
-  //   return (
-  //     <>
-  //       <div className="trial">
-  //         <Tab.Container
-  //           id="left-tabs-example"
-  //           defaultActiveKey="first"
-  //           className="no-scroll m-height"
-  //         >
-  //           <Row className="no-scroll m-height">
-  //             <Col sm={2} className="pill-tabs-color m-height">
-  //               <Nav variant="pills" className="flex-column pill-tabs">
-  //                 <Nav.Item>
-  //                   <Nav.Link eventKey="first">Home</Nav.Link>
-  //                 </Nav.Item>
-  //                 <Nav.Item>
-  //                   <Nav.Link eventKey="fourth">Onboarding Guide</Nav.Link>
-  //                 </Nav.Item>
-  //                 <Nav.Item>
-  //                   <Nav.Link eventKey="second">Your Deliveries</Nav.Link>
-  //                 </Nav.Item>
-  //                 <Nav.Item>
-  //                   <Nav.Link eventKey="third">Your Restaurants</Nav.Link>
-  //                 </Nav.Item>
-  //               </Nav>
-  //             </Col>
-  //             <Col sm={10} className="m-height">
-  //               <Tab.Content className="m-height">
-  //                 <Tab.Pane eventKey="first" className="m-height">
-  //                   <WelcomeMessage />
-  //                 </Tab.Pane>
-  //                 <Tab.Pane eventKey="fourth" className="m-height">
-  //                   <ProgramTraining />
-  //                 </Tab.Pane>
-  //                 <Tab.Pane eventKey="second" className="m-height">
-  //                   <div className="order-div">
-  //                     <Deliveries />
-  //                   </div>
-  //                 </Tab.Pane>
-  //                 <Tab.Pane eventKey="third" className="m-height">
-  //                   <RestaurantsPartners />
-  //                 </Tab.Pane>
-  //               </Tab.Content>
-  //             </Col>
-  //           </Row>
-  //         </Tab.Container>
-  //       </div>
-  //     </>
-  //   );
-  // }
+  componentWillUnmount() {
+    document.body.classList.remove("hunnid");
+    document.documentElement.classList.remove("hunnid");
+  }
 
   render() {
     return (
-      <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
-        <Tab eventKey="home" title="Home">
-          <WelcomeMessage />
-        </Tab>
-        <Tab eventKey="onboarding" title="Onboarding Guide">
-          <ProgramTraining />
-        </Tab>
-        <Tab eventKey="deliveries" title="Your Deliveries">
-          <div className="order-div">
-            <Deliveries />
-          </div>
-        </Tab>
-        <Tab eventKey="restaurants" title="Your Restaurants">
-            <RestaurantsPartners/>
-        </Tab>
-      </Tabs>
+      <>
+        <div>
+          <Tab.Container
+            id="left-tabs-example"
+            defaultActiveKey="first"
+            className="no-scroll m-height"
+          >
+            <Row className="no-scroll m-height">
+              <Col sm={2} className="pill-tabs-color trial">
+                <Nav variant="pills" className="flex-column pill-tabs">
+                  <Nav.Item>
+                    <Nav.Link eventKey="first">Home</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="fourth">Onboarding Guide</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="second">Your Deliveries</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link eventKey="third">Your Restaurants</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Col>
+              <Col sm={10}>
+                <Tab.Content className="m-height">
+                  <Tab.Pane eventKey="first" className="m-height">
+                    <WelcomeMessage name={this.state.name} />
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="fourth" className="m-height">
+                    <ProgramTraining />
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="second" className="m-height">
+                    <div className="order-div">
+                      <Deliveries />
+                    </div>
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="third" className="m-height">
+                    <RestaurantsPartners />
+                  </Tab.Pane>
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
+        </div>
+      </>
     );
   }
+
+  // render() {
+  //   return (
+  //     <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example">
+  //       <Tab eventKey="home" title="Home">
+  //         <WelcomeMessage />
+  //       </Tab>
+  //       <Tab eventKey="onboarding" title="Onboarding Guide">
+  //         <ProgramTraining />
+  //       </Tab>
+  //       <Tab eventKey="deliveries" title="Your Deliveries">
+  //         <div className="order-div">
+  //           <Deliveries />
+  //         </div>
+  //       </Tab>
+  //       <Tab eventKey="restaurants" title="Your Restaurants">
+  //           <RestaurantsPartners/>
+  //       </Tab>
+  //     </Tabs>
+  //   );
+  // }
 }
 
 export default ProgramUserPage;
--- react_app/src/components/ProgramUserPage/WelcomeMessage.js
+++ react_app/src/components/ProgramUserPage/WelcomeMessage.js
@@ -7,7 +7,7 @@ class WelcomeMessage extends React.Component {
   render() {
     return (
       <div id="WelcomeMessage">
-        <h1>Welcome SampleProgram!</h1>
+        <h1>{"Welcome "} {this.props.name} {"!"}</h1>
         <h2>
           Manage your weekly deliveries and see your partnered restaurants. To
           begin, choose an option from the menu on the left
